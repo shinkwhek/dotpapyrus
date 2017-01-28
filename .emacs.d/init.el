84de21fdd1214492592cec45f371f9518bc7aece
shinkwhek <klklpshin@gmail.com>
2017-01-29 02:36:13 +0900

    rm space

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -106,5 +106,7 @@
 (setq opam-share
       (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
 (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
-
 (require 'utop)
+
+;; ---- ---- yatex ---- ---- ;;
+(add-to-list 'load-path "~/.emacs.d/site-lisp/yatex")
