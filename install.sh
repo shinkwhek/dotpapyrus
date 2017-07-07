589e198d2a37f582f3b8094c62daca35b3fef583
shinkwhek <klklpshin@gmail.com>
2017-07-07 14:16:12 +0900

    :shit: [FIX] typo

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo -e "\e[31;1;4mset zshrc\e[m"
 cp ./.zshrc ~/
 
-if [ -d ~/.zplug ] then
+if [ -d ~/.zplug ]; then
   echo -e "\e[35;1;4mzplug\e[m \e[33;1mis already installed.\e[m"
 else
   echo -e "\e[32;1mInstall\e[m \e[35;1;4mzplug\e[m\e[32;1m.!!!\e[m"
@@ -13,7 +13,7 @@ fi
 echo -e "\e[31;1;4mset vimrc\e[m"
 cp ./.vimrc ~/
 
-if [ -d ~/.vim/bundle ] then
+if [ -d ~/.vim/bundle ]; then
   echo -e "\e[35;1;4m~/.vim/bundle\e[m \e[33;1mis already created,\e[m"
 else  
   echo -e "\e[31;1mcreate ~/.vim/bundle\e[m"
