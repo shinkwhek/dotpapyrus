4cd8ce1f2b341a8a46cfceaf50bceb52e1d64a6c
shinkwhek <klklpshin@gmail.com>
2017-08-29 01:19:42 +1000

    [FIX] Makefile and theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,10 +52,8 @@
 (setq make-backup-files nil) 
 (setq auto-save-default nil)
 
-;; ==== ==== ==== Color ==== ==== ==== ;;
-(load-theme 'adwaita t)
-
 ;;; === === === Package === === === ;;;
+(add-to-list 'load-path "~/.emacs.d/elisp") 
 (add-to-list 'load-path "~/.emacs.d/funs")
 (require 'shkw)
 (require 'shkw-latex)
@@ -68,19 +66,29 @@
 (define-auto-insert "\\.tex$" "shkw-latex-template.tex")
 
 ;; ---- ---- package ---- ---- ;;
-(require 'package) ;; You might already have this line
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
-  (add-to-list 'package-archives (cons "melpa" url) t))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+(require 'package)
 
+;; MELPAを追加
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; MELPA-stableを追加
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+;; Marmaladeを追加
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+;; Orgを追加
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+;; 初期化
+(package-initialize)
 ;; ---- ---- powerline ---- ---- ;;
 (require 'powerline)
 (powerline-center-theme)
+
+;; ==== ==== ==== Color ==== ==== ==== ;;
+(load-theme 'green-phosphor t)
+;;(load-theme 'base16-default-dark t)
 
 ;; ---- ---- auto-complete ---- ---- ;;
 (ac-config-default)
--- Makefile
+++ Makefile
@@ -14,21 +14,27 @@ install: Makefile
 zsh_update: $(ZSHRC)
 	echo -e "\e[31;1;4mset zshrc\e[m"
 	cp ./$(ZSHRC) ~/
-	zsh: Makefile $(ZSHRC)
+
+zsh: Makefile $(ZSHRC)
 	make zsh_update
 	echo -e "\e[32;1mInstall\e[m \e[35;1;4mzplug\e[m\e[32;1m.!!!\e[m"
 	curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 
+emacs_update_init_el: $(EMACS)/init.el
+	cp $(EMACS)/init.el ~/.emacs.d/
+
 emacs_update: $(EMACS)
 	echo -e "\e[31;1;4mset emacs\e[m"
 	cp -r $(EMACS) ~/
-	emacs: Makefile $(EMACS)
+
+emacs: Makefile $(EMACS)
 	make emacs_update
 
 vim_update: $(VIM)
 	echo -e "\e[31;1;4mset vimrc\e[m"
 	cp ./$(VIM) ~/
-	vim: Makefile $(VIM)
+
+vim: Makefile $(VIM)
 	make vim_update
 	echo -e "\e[31;1mcreate ~/.vim/bundle\e[m"
 	mkdir -p ~/.vim/bundle
@@ -41,4 +47,4 @@ vim_update: $(VIM)
 update: zsh_update emacs_update vim_update
 
 
-PHONY: all zsh emacs vim install update purge
+PHONY: all zsh emacs vim install update purge emacs_update_init_el
